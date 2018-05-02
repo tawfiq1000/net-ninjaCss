c86cfaec1ef0d4f9548b158fbbfd3e79b3b30e4a
Tawfiq <tawfiq.abir@gmail.com>
2018-05-02 11:49:14 +0600

    start the series

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CSS NET_NINJA</title>
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+</head>
+<body>
+	<div id="header">
+		<h1>Selectors</h1>
+		<h2>in css</h2>
+		<p>welcome to the website!!</p>
+
+		
+	</div>
+	<p>Hello there ninjas</p>
+
+</body>
+</html>
